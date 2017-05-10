e24ad306007b17e95a93fd1f8688688b7635abbb
daf276 <daf276@gmx.de>
2017-05-10 19:17:10 +0200

    not working, wat

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import os
+
+from django.shortcuts import render_to_response
 
 # Create your views here.
+from inaeternum.settings import BASE_DIR
 
 
 def index(request):
-    return render(request, '../code/StartInformation.html', {})
+    return render_to_response(os.path.join(BASE_DIR,'code','StartInformation.html'))
+    #return render(request, '../code/StartInformation.html', {})
     #return HttpResponse('Hello World')
