8936efc57910b4194b17dc6b17b0993f69b0b418
daf276 <daf276@gmx.de>
2017-05-10 19:34:00 +0200

    working?

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,12 @@
 import os
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 # Create your views here.
 from inaeternum.settings import BASE_DIR
 
 
 def index(request):
-    return render_to_response(os.path.join(BASE_DIR,'code','StartInformation.html'))
+    return render(request, os.path.join('code','StartInformation.html'))
     #return render(request, '../code/StartInformation.html', {})
     #return HttpResponse('Hello World')
