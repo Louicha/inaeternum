ad8c2acb60fbecff40c367ace83b95e34ce851bc
Louicha <inaeternum.dhbw@gmail.com>
2017-05-11 21:35:08 +0200

    Add new design
    Material Design
    TODO: remove links to index html

---
+++ polls/static/footer.php
@@ -0,0 +1,14 @@
+<footer class="mdl-mini-footer">
+  <div class="mdl-mini-footer__left-section">
+  <!--  <div class="mdl-logo">Title</div> -->
+    <ul class="mdl-mini-footer__link-list">
+      <li><a href="#">Help</a></li>
+      <li><a href="impressum.html">Impressum </a></li>
+      <li><a href="https://github.com/Louicha/inaeternuml">Github </a></li>
+    </ul>
+  </div>
+</footer>
+
+ </body>
+ 
+ </html>
