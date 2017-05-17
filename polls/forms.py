f23edbf3c99eacfb5c11e04904705a16d49f9d88
kageru <1be4b690ed03a17c0826dbcb46b8c071>
2017-05-17 12:42:21 +0200

    first attempt at creating a registration page

---
+++ polls/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class RegisterForm(forms.form):
+    first_name = forms.CharField(label='First Name')
+    last_name = forms.CharField(label='Last Name')
+    email = forms.EmailField(label='Your Email Address')
+    date_of_birth = forms.DateField(label='Date of Birth')
+    password = forms.PasswordInput()
+    password2 = forms.PasswordInput()
--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,21 @@
 import os
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-from inaeternum.settings import BASE_DIR
+# from inaeternum.settings import BASE_DIR
+from .forms import RegisterForm
 
 
 def index(request):
     return render(request, os.path.join('polls', 'static', 'StartInformation.html'))
-    # return render(request, '../code/StartInformation.html', {})
-    # return HttpResponse('Hello World')
 
 
 def register(request):
-    return HttpResponse('Can we just pretend that there\'s a register form on this page?<br>kthxbye')
-
-
-"""
-don't even ask.
-TODO: clean this up one day in the far future after I've received enough questions about it
-<20:24:15> "Attila": .-.
-<20:24:20> "kageru": ._.
-<20:24:21> "Attila": ._.
-<20:24:25> "Attila": .-.
-<20:24:29> "Attila": -.-
-<20:24:34> "Attila": .________.
-<20:24:36> "kageru": .__.
-<20:24:40> "Attila": *_____________*
-<20:24:42> "kageru": >.<
-<20:24:51> "Attila": ;>
-"""
+    # form data is send to the server
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('Registration successful')
+    # if a GET (or any other method), we'll create a blank form
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
